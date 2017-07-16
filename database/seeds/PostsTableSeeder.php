702b661fa6bd9ff277cee90081f350ed90d01f0b
Illia Morkva <morkva.illia@gmail.com>
2017-07-16 17:27:59 +0300

    display published posts only

--- database/seeds/PostsTableSeeder.php
+++ database/seeds/PostsTableSeeder.php
@@ -1,5 +1,6 @@
 <?php
 
+use Carbon\Carbon;
 use Illuminate\Database\Seeder;
 use Faker\Factory;
 
@@ -18,11 +19,14 @@ class PostsTableSeeder extends Seeder
         //generate 10 dummy posts data
         $posts = [];
         $faker = Factory::create();
+        $date = Carbon::create(2017, 7, 16, 9);
 
         for($i = 1; $i <= 10; $i++) {
 
             $image = "Post_Image_" . rand(1, 5) . ".jpg";
-            $date = date("Y-m-d H:i:s", strtotime("2017-07-16 08:00:00 + {$i} days"));
+            $date->addDays(1);
+            $publishedDate = clone($date);
+            $createdDate = clone($date);
 
             $posts[] = [
                 'author_id' => rand(1, 3),
@@ -31,8 +35,9 @@ class PostsTableSeeder extends Seeder
                 'body' => $faker->paragraphs(rand(10, 15), true),
                 'slug' => $faker->slug(),
                 'image' => rand(0, 1) == 1 ? $image : null,
-                'created_at' => $date,
-                'updated_at' => $date,
+                'created_at' => $createdDate,
+                'updated_at' => $createdDate,
+                'published_at' => $i < 5 ? $publishedDate : ( rand(0, 1) == 0 ? null : $publishedDate->addDays(4) )
             ];
         }
 
