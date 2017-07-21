ba091a3777d48e76596e41f1c997a65f6d2169d3
Illia Morkva <morkva.illia@gmail.com>
2017-07-21 15:58:36 +0300

    count the post view & tidy up the composer service provider

--- app/Providers/ComposerServiceProvider.php
+++ app/Providers/ComposerServiceProvider.php
@@ -2,9 +2,8 @@
 
 namespace App\Providers;
 
-use App\Post;
 use Illuminate\Support\ServiceProvider;
-use App\Category;
+use App\Views\Composers\NavigationComposer;
 
 class ComposerServiceProvider extends ServiceProvider
 {
@@ -15,18 +14,7 @@ class ComposerServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        view()->composer('layouts.sidebar', function($view){
-            $categories = Category::with(['posts' => function($query) {
-                $query->published();
-            }])->orderBy('title', 'asc')->get();
-
-            return $view->with('categories', $categories);
-        });
-
-        view()->composer('layouts.sidebar', function($view){
-            $popularPosts = Post::published()->popular()->take(3)->get();
-            return $view->with('popularPosts', $popularPosts);
-        });
+        view()->composer('layouts.sidebar', NavigationComposer::class);
     }
 
     /**
